edc32565547791d4ba5659d00319dc8c1f4807de
MuttiD <dgmutti@hotmail.com>
2023-02-15 13:40:11 +0000

    Create template to render the view

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Review
+from .models import CarReview
 
 
 class ReviewList(generic.ListView):
-    model = Review    
-    queryset = Review.objects.filter(status=1).order_by('-created_on')
+    model = CarReview
+    queryset = CarReview.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6         # limit the nr of reviews
