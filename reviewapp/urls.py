ac9ffda171c49b8f1ba8e57e5f63bfbd7bf166fc
MuttiD <dgmutti@hotmail.com>
2023-09-04 00:52:47 +0000

    Fix detail review page

--- reviewapp/urls.py
+++ reviewapp/urls.py
@@ -1,14 +1,13 @@
 from . import views
 from django.urls import path
-from .views import ReviewDetailView
+# from .views import ReviewDetailView
 
 urlpatterns = [
     path('', views.ReviewDetailList.as_view(), name='all_reviews'),
-    path('<slug:slug>/', views.ReviewDetailView.as_view(),
-         name='review_detail'),
-    path('review_comments', views.ReviewCommentView.as_view(),
-         name='review_comments'),
-    path('like/<slug:slug>/', views.ReviewLike.as_view(), name='review_like'),
-    path('<slug:slug>/', views.ReviewLike.as_view(),
-         name='review_detail'),
+    path('<slug:slug>/', views.review_detail, name='review_detail'),    
+    path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete,
+         name='comment_delete'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.update_comment,
+         name='update_comment'),
+    path('like/<slug:slug>', views.review_like, name='review_like')
 ]
--- reviewapp/views.py
+++ reviewapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views.generic import View, DetailView, ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from .models import CarReviewModel, CarCommentModel
 from django.http import HttpResponseRedirect
 from .forms import CommentForm
@@ -18,87 +18,103 @@ class ReviewDetailList(ListView):
     # context_object_name = 'reviews'
 
 
-class ReviewDetailView(DetailView):
+def review_detail(request, slug, *args, **kwargs):
     """
-    Returns full detail of the review
+    This function-based view will allow users to view
+    the detail of a review.    
     """
 
-    def get(self, request, slug, *args, **kwargs):
-        queryset = CarReviewModel.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comment_user.filter(approved=True).order_by(
-                                            'created_on')
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+    queryset = CarReviewModel.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments_by_user.all().order_by("-created_on")
+    comment_count = post.comments_by_user.filter(approved_by_admin=True).count()
+    liked = False
+    commented = False
 
-        return render(
-            request,
-            "review_details.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": False,
-                "liked": liked,
-                "CommentForm": CommentForm,
-            },
-        )
+    if post.likes.filter(id=request.user.id).exists():
+        liked = True
 
-    def post(self, request, slug, *args, **kwargs):
-        """
-        Returns details with approved reviews
-        Logged in users can submit a comment for approval
-        """
-        queryset = CarReviewModel.objects.filter(status=1)
-        review = get_object_or_404(queryset, slug=slug)
-        comments = review.objects.filter(
-            approved=True).order_by('created_on')
-        liked = False
-        if review.likes.filter(id=self.request.user.id).exists():
-            liked = True
-
+    if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = review
-            comment.save()
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Comment awaiting moderation.')
         else:
             comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
 
-        return render(
-            request,
-            "review_details.html",
-            {
-                "review": review,
-                "comments": comments,
-                "commented": True,
-                "liked": liked,
-                "comment_form": comment_form,
-            },
-        )
+    return render(
+        request,
+        "review_details.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "liked": liked,
+            "comment_form": comment_form
+        },
+    )
 
 
-class ReviewCommentView(View):
+def update_comment(request, review_id, *args, **kwargs):
     """
-    Once the user is logged in, it will allow
-    the user to create a comment
+    This view will allow users to update their comments 
     """
-    template_name = "review_comments.html"
-    form_class = CommentForm
+    if request.method == "POST":
 
+        queryset = CarReviewModel.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = post.comments.filter(id=comment_id).first()
 
-class ReviewLike(View):
-    """
-    Review likes counted. Total likes displayed
-    """
-    def post(self, request, slug):
-        review = get_object_or_404(CarReviewModel, slug=slug)
-        if review.likes.filter(id=request.user.id).exists():
-            review.likes.remove(request.user)
+        comment_form = CommentForm(data=request.POST, instance=new_comment)
+        if comment_form.is_valid() and comment.name == request.user.username:
+            update_comment = comment_form.save(commit=False)
+            update_comment.post = post
+            update_comment.approved = False
+            update_comment.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Comment Updated!')
         else:
-            review.likes.add(request.user)
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
 
-        return HttpResponseRedirect(reverse('review_detail',
-                                    args=[review.slug]))
+    return HttpResponseRedirect(reverse('review_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id, *args, **kwargs):
+    """
+    A view to delete user's comment
+    """
+    queryset = CarCommentModel.objects.filter(status=1)
+    post = get_object_or_404(queryset)
+    comment = post.comments.filter(id=comment_id).first()
+
+    if comment.name == request.user.username:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS,
+                             'Your comment has been deleted!')
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('review_detail', args=[slug]))
+
+
+def review_like(request, slug, *args, **kwargs):
+    """
+    Review likes counted. Total likes will be displayed
+    """
+    post = get_object_or_404(CarReviewModel, slug=slug)
+    if request.method == "POST" and request.user.is_authenticated:
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+    return HttpResponseRedirect(reverse('review_details', args=[slug]))
