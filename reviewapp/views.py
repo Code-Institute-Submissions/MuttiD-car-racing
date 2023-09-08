b06641466a73b6ab4258445429b649f4e5f550b5
MuttiD <dgmutti@hotmail.com>
2023-09-08 05:36:11 +0000

    Fix logic & issues on the comments functionalities

--- reviewapp/views.py
+++ reviewapp/views.py
@@ -25,8 +25,10 @@ def review_detail(request, slug, *args, **kwargs):
 
     queryset = CarReviewModel.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    comments = post.comments_by_user.filter(approved_by_admin=True).order_by("-created_on")
-    comment_count = post.comments_by_user.filter(approved_by_admin=True).count()
+    comments = post.comments_by_user.filter(
+        approved_by_admin=True).order_by("-created_on")
+    comment_count = post.comments_by_user.filter(
+        approved_by_admin=True).count()
     liked = False
     commented = False
 
@@ -41,6 +43,7 @@ def review_detail(request, slug, *args, **kwargs):
             comment_form.instance.comment_user = post
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
+            new_comment.review = post
             new_comment.save()
             messages.add_message(request, messages.SUCCESS,
                                  'Comment awaiting moderation.')
@@ -66,17 +69,22 @@ def update_comment(request, comment_id):
     """
     This view will allow users to update their comments
     """
-
-    update_c = CarCommentModel.objects.get(pk=comment_id)
+    context = {}
+    update_c = get_object_or_404(CarCommentModel, id=comment_id)
     update_form = CommentForm(request.POST or None, instance=update_c)
 
-    if update_c.comment_user.author != request.user:
-        return redirect("all_reviews")
-
-    if update_form.is_valid():  # Fixed the form variable name
+    if update_form.is_valid():
+        if update_c.comment_user.author != request.user:
+            messages.success(
+                request, "You do not have permission to update this comment")
+            return redirect("all_reviews")
         update_form.save()
-        return redirect("all_reviews")
-    return render(request, "update_comment.html", {'update_form': update_form})
+        return redirect("review_detail", update_c.review.slug)
+
+    context["form"] = update_form
+    context["update_c"] = update_c
+
+    return render(request, "update_comment.html", context)
 
 
 def delete_comment(request, comment_id):
@@ -87,26 +95,25 @@ def delete_comment(request, comment_id):
     context = {'delete_c': delete_c}
 
     if request.method == 'POST':
-        if delete_c.comment_user.author != request.user:  # Fixed the condition
+        if delete_c.comment_user.author != request.user:
+            messages.success(
+                request, "You do not have permission to delete this comment")
             return redirect("all_reviews")
 
         delete_c.delete()
-        messages.success(request,
-                         ("You have deleted this comment"))
-        return redirect("all_reviews")
-
+        messages.success(request, "You have deleted this comment")
+        return redirect(reverse("review_detail",
+                                kwargs={'slug': delete_c.review.slug}))
     return render(request, "delete_comment.html", context)
 
 
 class ContactView(FormView):
+    """
+    A class based view to render Contacts
+    """
     template_name = 'contact.html'
     form_class = ContactForm
     success_url = '/success'
-
-    def form_valid(self, form):
-        form.save()    
-        messages.success(self.request, 'Thank you for your message. We will get back to you soon!')    
-        return super().form_valid(form)
 
     def get(self, request):
         form = ContactForm()
@@ -116,12 +123,14 @@ class ContactView(FormView):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Thank you for your message. We will get back to you soon!')
             return redirect('success')
         return render(request, 'contact.html', {'form': form})
 
 
 def success(request):
+    """
+    A view to render the success of a contact submitted
+    """
     return render(request, 'success.html')
 
 
